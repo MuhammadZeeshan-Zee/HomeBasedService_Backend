838fa314e1c1617e28a037fe64236168590e0b3b
zeeshan <zeeshanarif34101@gmail.com>
2024-11-25 08:09:29 +0500

    origin is updated

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,11 +3,11 @@ import express from "express";
 import cors from "cors";
 const app = express();
 const corsOptions = {
-    origin: ['https://home-based-service-frontend.vercel.app', 'http://localhost:3000'], // Allow deployed frontend and localhost (for testing)
+    origin:  process.env.ORIGIN, // Allow deployed frontend and localhost (for testing)
     credentials: true, // If you need to send cookies or auth headers
   };
   
-app.use(cors(corsOptions)); //request is vailed from which origin
+app.use('*',cors(corsOptions)); //request is vailed from which origin
 app.use(express.json({ limit: "16kb" })); //to get data in json and also give the data limit
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); //to get data from url and also give the data limit
 app.use(express.static("public")); //serve static assets, this data can access everyone
